c6f6c3955cff823d97222d9a3bfe1dd52265ac8f
kaimoe <kimon.kar@protonmail.com>
2021-02-27 18:50:51 +0200

    Vim correction

--- vim.sh
+++ vim.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+sed -i 's/vnoremap \$/"vnoremap \$/g' ~/.vim_runtime/vimrcs/extended.vim
+sed -i 's/inoremap \$/"inoremap \$/g' ~/.vim_runtime/vimrcs/extended.vim
+
 echo "set number
 set mouse=a" >> ~/.vimrc
 
