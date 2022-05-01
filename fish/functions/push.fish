7a688e90b443a6fe1c424631b95bb3b00317db45
Kim Kar <kimon.kar@protonmail.com>
2022-05-01 14:38:57 +0300

    Update fish/push

--- fish/functions/push.fish
+++ fish/functions/push.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function push --wraps='git push -u origin main' --description 'alias push git push -u origin main'
-  git push -u origin main $argv;
+  git push origin -u $argv;
 end
